3611fd7677054589574602d474a9d7026192b0ce
Francesca di Centa <dicenta.francesca@gmail.com>
2017-07-10 21:01:30 +0200

    basic content and scheleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  get '', to:'pages#home'
+
+  get 'french', to: 'pages#french'
+
+  get 'deutsch', to: 'pages#deutsch'
+
+  get 'italian', to: 'pages#italiano'
+
+  get 'spanish', to: 'pages#spanish'
+
+  get 'contact', to: 'pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
